61747bc27ac2a6b9961a806def9cd9b86ecf0573
Andy Fitzgerald <andy@andyfitzgeraldconsulting.com>
2021-04-03 12:44:02 -0700

    Add ETL pipeline to Gulp and Fuseki as data source
    Reasoning on Fuseki server still not working. Debug on a new branch.

--- gulpfile.js
+++ gulpfile.js
@@ -12,25 +12,49 @@ const browserSync = require('browser-sync');
 
 // Build Jekyll site (but do not compile CSS)
 function jekyll (gulpCallBack){
-    const jekyll = spawn('jekyll', ['build'], {stdio: 'inherit'});
+    const jekyll = spawn('jekyll', ['build', '--trace'], {stdio: 'inherit'});
     jekyll.on('exit', function(code) {
         gulpCallBack(code === 0 ? null : 'ERROR: Jekyll process exited with code: '+code);
     });
 }
 
-// Build Methods RDF from .gsheet with Methods.sparql
-function methods (gulpCallBack){
+// Extract Methods RDF from .gsheet with Methods.sparql
+function ext_methods (gulpCallBack){
     const methods = exec('tarql ~/repos/uxmd/_data/etl/Methods.sparql > ~/repos/uxmd/_data/etl/Methods.ttl', {stdio: 'inherit'});
     methods.on('exit', function(code) {
         gulpCallBack(code === 0 ? null : 'ERROR: Methods Tarql process exited with code: '+code);
     });
 }
 
-// Build WebResources RDF from .gsheet with WebResources.sparql
-function resources (gulpCallBack){
+// Extract WebResources RDF from .gsheet with WebResources.sparql
+function ext_resources (gulpCallBack){
     const resources = exec('tarql ~/repos/uxmd/_data/etl/WebResources.sparql > ~/repos/uxmd/_data/etl/WebResources.ttl', {stdio: 'inherit'});
     resources.on('exit', function(code) {
         gulpCallBack(code === 0 ? null : 'ERROR: Resources Tarql process exited with code: '+code);
+    });
+}
+
+// Load UXM Ontology to Fuseki (replace everything in dataset)
+function load_ontology (gulpCallBack){
+    const ontology = exec('s-put http://localhost:3030/UXM https://www.uxmethods.org/g1 ~/repos/uxmd/_data/etl/UXMethodsKG.owl', {stdio: 'inherit'});
+    ontology.on('exit', function(code) {
+        gulpCallBack(code === 0 ? null : 'ERROR: Methods SOH process exited with code: '+code);
+    });
+}
+
+// Load Methods data to Fuseki
+function load_methods (gulpCallBack){
+    const methods = exec('s-post http://localhost:3030/UXM default ~/repos/uxmd/_data/etl/Methods.ttl', {stdio: 'inherit'});
+    methods.on('exit', function(code) {
+        gulpCallBack(code === 0 ? null : 'ERROR: Methods SOH process exited with code: '+code);
+    });
+}
+
+// Load WebResources data to Fuseki
+function load_resources (gulpCallBack){
+    const resources = exec('s-post http://localhost:3030/UXM default ~/repos/uxmd/_data/etl/WebResources.ttl', {stdio: 'inherit'});
+    resources.on('exit', function(code) {
+        gulpCallBack(code === 0 ? null : 'ERROR: Resources SOH process exited with code: '+code);
     });
 }
 
@@ -68,7 +92,7 @@ function reload (done) {
   done();
 }
 
-// Watch files
+// Watch Jekyll files
 function watch() {
   gulp.watch("_sass/*.scss", sass);
   gulp.watch(
@@ -79,13 +103,29 @@ function watch() {
       "serviceworker.js",
       "**/*.md",
       "_config.yml",
-      "!_data/**",
-      "_data/UXMethods.owl"
+      "!_data/**"
     ],
     gulp.series(jekyll, reload)
   );
 }
 
+// Watch ontology
+function watch_data() {
+  gulp.watch(
+    "_data/etl/UXMethodsKG.owl",
+    gulp.series(load_ontology, load_methods, load_resources, jekyll, reload)
+  );
+}
+
 // Export tasks
+// `gulp` builds UI from MD & SPARQL endpoint data and watches for changes to .md and template files. Fuseki must be running.
+// USE FOR: any work on app.
 exports.default = gulp.series(jekyll, sass, gulp.parallel(watch, php));
-exports.etl = gulp.series(methods, resources);
+
+// `gulp etl` runs Tarql extraction, loads ontology & data into Fuseki, and watches for changes to the ontology. Fuseki must be running.
+// USE FOR: updating data, iterating ontology.
+exports.etl = gulp.series(ext_methods, ext_resources, load_ontology, load_methods, load_resources, watch_data);
+
+// `gulp update` runs Tarql extraction and loads ontology & data into Fuseki.
+// USE FOR: updating data from Google Sheets sources only
+exports.update = gulp.series(ext_methods, ext_resources, load_ontology, load_methods, load_resources, jekyll, reload);
